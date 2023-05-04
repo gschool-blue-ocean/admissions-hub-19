cbbfbb3966824daa254621db2305c8ac65979c61
crose275 <crose275@gmail.com>
2023-05-04 12:47:38 -0700

    started migration data

--- api/src/db/migration.sql
+++ api/src/db/migration.sql
@@ -1,6 +1,41 @@
 DROP TABLE IF EXISTS tasks;
 
-CREATE TABLE tasks (
-  id SERIAL PRIMARY KEY,
-  description TEXT
-);
+CREATE TABLE IF NOT EXISTS users(
+  id serial PRIMARY KEY,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  email varchar(150) NOT NULL,
+  isStaff boolean NOT NULL,
+  password_hash varchar(100) NOT NULL,
+  salt BYTEA NOT NULL
+) 
+
+CREATE TABLE IF NOT EXISTS students (
+  id serial PRIMARY KEY NOT NULL,
+  user_id int NOT NULL,
+  cohort_id int,
+  numAttempts int,
+  paid boolean,
+  paperwork1 boolean,
+  paperwork2 boolean,
+  paperwork3 boolean
+)
+
+/*CREATE TABLE IF NOT EXISTS cohorts (
+
+) */
+
+/*CREATE TABLE IF NOT EXISTS questions (
+
+)*/
+
+/*CREATE TABLE IF NOT EXISTS question-notes (
+
+)*/
+
+/*CREATE TABLE IF NOT EXISTS attempts (
+
+)*/
+
+
+
