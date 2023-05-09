303ff54133028e74338aa75b96178fda66960453
kariT37 <karitinsley57@gmail.com>
2023-05-09 16:28:52 -0600

    Second dummy slide added

--- client/src/components/ImageCarousel.jsx
+++ client/src/components/ImageCarousel.jsx
@@ -4,9 +4,9 @@ import CarouselSample1 from '../Images/CarouselSample1.png';
 
 const ImageCarousel = () => {
     return (
-        <Carousel>
+        <Carousel showIndicators showControls>
             <Carousel.Item>
-                <image
+                <img
                     className='d-block w-100'
                     src={CarouselSample1}
                     alt='FirstSlide'
@@ -14,6 +14,17 @@ const ImageCarousel = () => {
                 <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                   className='d-block w-100'
+                   src={CarouselSample1}
+                   alt='SecondSlide'
+                />
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
