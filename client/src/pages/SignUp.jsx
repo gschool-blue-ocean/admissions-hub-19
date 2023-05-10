8da7ea9d43f14ea5bbed473c73d139851c9cbcd4
Fortun Joseph Binghay <fortunbing@yahoo.com>
2023-05-10 16:02:18 -0500

    trying to make signup page work

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUp = () => {
-    return <h2>Sign Up</h2>;
+
+  const routeHTTP = "http://localhost:8000/users";
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [userType, setUserType] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
   };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleUserTypeChange = (event) => {
+    setUserType(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      is_staff: userType === "staff",
+      password_hash: password,
+      salt: "salt", 
+    };
+
+    fetch(routeHTTP, {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input type="text" value={firstName} onChange={handleFirstNameChange} />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input type="text" value={lastName} onChange={handleLastNameChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <label>
+          Are you a student or staff member?
+          <br />
+          <label>
+            <input
+              type="radio"
+              value="student"
+              checked={userType === "student"}
+              onChange={handleUserTypeChange}
+            />
+            Student
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              value="staff"
+              checked={userType === "staff"}
+              onChange={handleUserTypeChange}
+            />
+            Staff
+          </label>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SignUp;
