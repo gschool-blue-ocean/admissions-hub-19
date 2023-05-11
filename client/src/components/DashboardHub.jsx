cec8c3d11daf0958c2ef38c1f8b92d3db4c47351
kariT37 <karitinsley57@gmail.com>
2023-05-11 11:26:52 -0600

    Added table and made some alignment adjustments

--- client/src/components/DashboardHub.jsx
+++ client/src/components/DashboardHub.jsx
@@ -1,20 +1,67 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 import '../css/DashboardHub.css';
 
 const DashboardHub = () => {
     return (
-    <div className="DashboardHub">
-        <Button className='FilterBtn' variant="primary">Filter</Button>
-        <Button className='AddStudentBtn' variant="primary">Add Student</Button>
-        <Button className='DeleteStudentBtn' variant="primary">Delete Student</Button>
-        <Button className='UpdateStudentBtn' variant="primary">Update Student</Button>
-        <Button className='LaunchInterviewBtn' variant="primary">Launch Interview</Button>
-        <Button className='ExportStudentBtn' variant="primary">Export Student</Button>
+        <div className="DashboardHub">
+            <Button className='FilterBtn' variant="primary">Filter</Button>
+            <Table borderedless hover className="Table">
+                <thead>
+                    <tr>
+                        <th>Student Name</th>
+                        <th>Email Address</th>
+                        <th>Cohort</th>
+                        <th>Last Interview</th>
+                        <th>Attempt</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Joe Johnson</td>
+                        <td>IsoJoe@hotmail.com</td>
+                        <td>MCSP-19</td>
+                        <td>August 28, 2022</td>
+                        <td>1</td>
+                        <td>Retired</td>
+                    </tr>
+                    <tr>
+                        <td>Joe Johnson</td>
+                        <td>IsoJoe@hotmail.com</td>
+                        <td>MCSP-19</td>
+                        <td>August 28, 2022</td>
+                        <td>1</td>
+                        <td>Retired</td>
+                    </tr>
+                    <tr>
+                        <td>Joe Johnson</td>
+                        <td>IsoJoe@hotmail.com</td>
+                        <td>MCSP-19</td>
+                        <td>August 28, 2022</td>
+                        <td>1</td>
+                        <td>Retired</td>
+                    </tr>
+                    <tr>
+                        <td>Joe Johnson</td>
+                        <td>IsoJoe@hotmail.com</td>
+                        <td>MCSP-19</td>
+                        <td>August 28, 2022</td>
+                        <td>1</td>
+                        <td>Retired</td>
+                    </tr>
+                </tbody>
+            </Table>
+            <Button className='AddStudentBtn' variant="primary">Add Student</Button>
+            <Button className='DeleteStudentBtn' variant="primary">Delete Student</Button>
+            <Button className='UpdateStudentBtn' variant="primary">Update Student</Button>
+            <Button className='LaunchInterviewBtn' variant="primary">Launch Interview</Button>
+            <Button className='ExportStudentBtn' variant="primary">Export Student</Button>
 
 
-    </div>
+        </div>
     );
-  };
+};
 
 export default DashboardHub;
