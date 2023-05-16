9c7f9f4293258f8d23894528b72e891365ca6180
AmoraMas <amoramas1984@gmail.com>
2023-05-16 14:32:08 -0600

    connected to DB

--- client/src/pages/Interview.jsx
+++ client/src/pages/Interview.jsx
@@ -1,13 +1,28 @@
 import React from "react";
-import InputNotes from "../components/InputNotes"
+import InputNotes from "../components/InputNotes";
+import CodingWindow from "../components/CodingWindow";
 
 const Interview = () => {
-  
   return (
-  <div>
-    <InputNotes/>
-  </div>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        position: "relative",
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      <CodingWindow
+        style={{
+          width: "1000px",
+        }}
+      />
+      <InputNotes style={{ width: "500px" }} />
+    </div>
   );
-}
+};
 
 export default Interview;
