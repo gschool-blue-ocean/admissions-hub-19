3b9f65eb84e54d87ffae61550ac37028a75590f7
AmoraMas <amoramas1984@gmail.com>
2023-05-18 13:14:42 -0600

    Student Selector needs CSS

--- client/src/components/StudentSelector.jsx
+++ client/src/components/StudentSelector.jsx
@@ -1,100 +1,118 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
 import { Container, Row, Col, Button, Nav } from "react-bootstrap";
 import baseurl from "../url";
 
-function StudentSelector() {
-    const [allCohorts, setAllCohorts] = useState([]);
-    const [allStudents, setAllStudents] = useState([]);
-    const [cohort, setCohort] = useState(0);
-    const [student, setStudent] = useState(0);
+function StudentSelector({ updateStudent }) {
+  const [allCohorts, setAllCohorts] = useState([]);
+  const [allStudents, setAllStudents] = useState([]);
+  const [cohort, setCohort] = useState(0);
+  const [student, setStudent] = useState(0);
 
-    const getAllCohorts = () => {
-        fetch(`${baseurl}/cohorts`, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-            mode: "cors",
-          })
+  const getAllCohorts = () => {
+    fetch(`${baseurl}/cohorts`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log("data:", data);
+        setAllCohorts(data);
+      });
+  };
+
+  const getAllStudents = () => {
+    fetch(`${baseurl}/students`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log("data:", data);
+        setAllStudents(data);
+      });
+  };
+
+  const handleCohortSelection = (event) => {
+    setCohort(event.target.value);
+    if (event.target.value != 0) {
+      fetch(`${baseurl}/students/cohort/${event.target.value}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        mode: "cors",
+      })
         .then((response) => response.json())
         .then((data) => {
-            console.log("data:", data);
-            setAllCohorts(data);
+          //console.log("data:", data);
+          setAllStudents(data);
         });
+    } else {
+      getAllStudents();
     }
+  };
 
-    const getAllStudents = () => {
-        fetch(`${baseurl}/students`, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-            mode: "cors",
-          })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("data:", data);
-            setAllStudents(data);
-        });
-    }
+  const handleStudentSelection = async (event) => {
+    setStudent(event.target.value);
+    await fetch(`${baseurl}/student/${event.target.value}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log("data:", data);
+        updateStudent(data);
+      });
+  };
 
-    const handleCohortSelection = (event) => {
-        setCohort(event.target.value);
-        if (event.target.value != 0) {
-            fetch(`${baseurl}/students/cohort/${event.target.value}`, {
-                method: "GET",
-                headers: { "Content-Type": "application/json" },
-                mode: "cors",
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("data:", data);
-                setAllStudents(data);
-            });
-        } else {
-            getAllStudents();
-        }
-    }
-
-    const handleStudentSelection = (event) => {
-        setStudent(event.target.value);
-    }
-
-    useEffect(() => {
-        getAllCohorts();
-        getAllStudents();
-      }, []);
+  useEffect(() => {
+    getAllCohorts();
+    getAllStudents();
+  }, []);
 
   return (
     <div>
-        <div className="cohort-select">
-            <label>Cohort</label>
-            <select name="cohort-select" id="cohort-select">
-                <option value="0">All Cohorts</option>
-                {allCohorts.map((cohort, index) => {
-                    <option
-                        key={index}
-                        value={cohort.cohort_id}
-                        onClick={handleCohortSelection}
-                    >
-                        {cohort.name}
-                    </option>
-                })}
-            </select>
-        </div>
-        <div className="student-select">
-            <label>Student</label>
-            <select name="student-select" id="student-select">
-                <option value="0">All Students</option>
-                {allStudents.map((student, index) => {
-                    <option
-                        key={index}
-                        value={student.student_id}
-                        onClick={handleStudentSelection}
-                    >
-                        {student.first_name} {student.last_name}
-                    </option>
-                })}
-            </select>
-        </div>
-
+      <div className="cohort-select">
+        {/* <label>Cohort</label> */}
+        <select
+          name="cohort-select"
+          id="cohort-select"
+          onChange={handleCohortSelection}
+        >
+          <option key={0} value="0">
+            All Cohorts
+          </option>
+          {allCohorts.map((cohort) => {
+            return (
+              <option key={cohort.cohort_id} value={cohort.cohort_id}>
+                {cohort.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="student-select">
+        {/* <label>Student</label> */}
+        <select
+          name="student-select"
+          id="student-select"
+          onChange={handleStudentSelection}
+        >
+          <option key={0} value="0">
+            Select Student
+          </option>
+          {allStudents.map((student) => {
+            return (
+              <option key={student.student_id} value={student.student_id}>
+                {student.first_name} {student.last_name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      {/*
         <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownCohorts" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Cohorts
         </button>
@@ -111,6 +129,7 @@ function StudentSelector() {
                 <a className="dropdown-item" key={index} value={student.student_id} onClick={handleStudentSelection}>{cohort.name}</a>
             })}
         </div>
+        */}
     </div>
   );
 }
