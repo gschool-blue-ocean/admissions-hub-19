4d935ec201a2b56ff0b1f40f54b9a219095367d6
Javaney <javaneyt@icloud.com>
2023-05-21 19:24:09 -0400

    Monaco Editor addition

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Editor from "@monaco-editor/react";
+import CodeParent from "./CodeParent";
 
 const CodeEditor = ({ onChange, language, code, theme }) => {
   const [value, setValue] = useState(code || "");
@@ -12,6 +13,7 @@ const CodeEditor = ({ onChange, language, code, theme }) => {
 
   return (
     <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+      <CodeParent />
       <Editor
         height="100vh"
         width={`100%`}
@@ -21,7 +23,9 @@ const CodeEditor = ({ onChange, language, code, theme }) => {
         defaultValue="// Editor view should match problem set"
         onChange={handleEditorChange}
       />
+
     </div>
+
   );
 };
 export default CodeEditor;
