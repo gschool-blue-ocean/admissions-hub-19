a34e8a5faaa1db66779285a2839dcba7dc3f5388
joegor85 <joe.gor85@gmail.com>
2023-06-09 12:27:30 -0500

    removed 'salt' instances but still need them in the encryption algorithm for it to work. Now salts are not being stored in the database for new users

--- api/src/routesUsers.js
+++ api/src/routesUsers.js
@@ -11,13 +11,10 @@ const db = new pg.Pool({
 });
 
 function generateHash(password) {
-
-  const salt = crypto.randomBytes(16).toString('hex'); // Generate a random salt
-
   const hash = crypto
-    .pbkdf2Sync(password, salt, 10000, 64, 'sha512') // Perform the hashing algorithm (pbkdf2Sync)
+    .pbkdf2Sync(password, 'salt', 10000, 64, 'sha512') // Perform the hashing algorithm (pbkdf2Sync)
     .toString('hex'); // Convert the hash to a hexadecimal string
-  return { salt, hash };
+  return { hash };
 }
 
 async function findAll(_req, res, next) {
@@ -42,8 +39,8 @@ async function findOne(req, res, next) {
 
 async function create(req, res, next) {
   const { first_name, last_name, email, password } = req.body;
-  const keys = "first_name, last_name, email, salt, password_hash";
-  const {salt, hash} = generateHash(password)
+  const keys = "first_name, last_name, email, password_hash";
+  const {hash} = generateHash(password)
   //console.log("CREATE USER BODY:", req.body);
   if (
     first_name === undefined ||
@@ -65,11 +62,10 @@ async function create(req, res, next) {
       //   bcrypt.hash(password_hash, salt, function(err, hash) {
       //   return hash});
       //   })
-
       const result = await db
         .query(
-          `INSERT INTO users (${keys}) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-          [first_name, last_name, email, Buffer.from(salt, 'hex'), hash]
+          `INSERT INTO users (${keys}) VALUES ($1, $2, $3, $4) RETURNING *`,
+          [first_name, last_name, email, hash]
         )
         .catch(next);
       res.send(result.rows[0]);
@@ -101,7 +97,6 @@ async function update(req, res, next) {
     "first_name",
     "last_name",
     "email",
-    "salt",
     "password_hash",
   ];
   for (let key in req.body) {
@@ -151,9 +146,8 @@ async function authenticate(req, res, next) {
   }
 
   const storedHash = result.rows[0].password_hash;
-  const storedSalt = result.rows[0].salt.toString('hex');
 
-  const inputHash = crypto.pbkdf2Sync(password, Buffer.from(storedSalt), 10000, 64, 'sha512').toString('hex');
+  const inputHash = crypto.pbkdf2Sync(password, 'salt', 10000, 64, 'sha512').toString('hex');
   if (inputHash === storedHash) {
     const payload = 
     { email: result.rows[0].email,
