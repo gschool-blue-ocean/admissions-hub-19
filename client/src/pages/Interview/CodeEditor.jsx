9d519fe6712035799a4cea06814c0edc053893f3
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-06-09 18:25:29 -0500

    code sandbox isntances

--- client/src/pages/Interview/CodeEditor.jsx
+++ client/src/pages/Interview/CodeEditor.jsx
@@ -2,56 +2,101 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref } from "firebase/database";
-// import "codemirror";
-// import Firepad, {
-//   IDatabaseAdapter,
-//   IEditorAdapter,
-//   IFirepadConstructorOptions,
-// } from "@hackerrank/firepad";
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackPreview,
+  SandpackCodeEditor,
+  SandpackConsole,
+  useSandpack,
+} from "@codesandbox/sandpack-react";
 
 function CodeEditor() {
-  const [config, setConfig] = useState({
-    apiKey: import.meta.env.VITE_apiKey,
-    authDomain: import.meta.env.VITE_authDomain,
-    databaseURL: import.meta.env.VITE_databaseURL,
-    projectId: import.meta.env.VITE_projectID,
-    storageBucket: import.meta.env.VITE_storageBucket,
-    messagingSenderId: import.meta.env.VITE_messagingSenderId,
-    appId: import.meta.env.VITE_appID,
-  });
-  const [firepadRef, setFirePadRef] = useState(null);
-  const [firebaseApp, setFirebaseApp] = useState(initializeApp(config));
-  const [db, setDb] = useState(getDatabase(firebaseApp));
-  const [dbRef, setDbRef] = useState(ref(db));
-  // template for generating session keys
-  // console.log(dbRef);
-
-  // const getExampleRef = () => {
-  // var ref = getDatabase().ref();
-  //   var hash = window.location.hash.replace(/#/g, "");
-  //   if (hash) {
-  //     ref = ref.child(hash);
-  //   } else {
-  //     ref = ref.push(); // generate unique location.
-  //     window.location = window.location + "#" + ref.key; // add it as a hash to the URL.
-  //   }
-  //   if (typeof console !== "undefined") {
-  //     console.log("Firebase data: ", ref.toString());
-  //   }
-  //   return ref;
+  const files = {
+    "/index.js": `console.log("test")`,
+  };
+  // const handleChange = (input) => {
+  //   console.log(input);
   // };
+  // for the time being firebase will be out until websocket solution is made
+  // const [config, setConfig] = useState({
+  //   apiKey: import.meta.env.VITE_apiKey,
+  //   authDomain: import.meta.env.VITE_authDomain,
+  //   databaseURL: import.meta.env.VITE_databaseURL,
+  //   projectId: import.meta.env.VITE_projectID,
+  //   storageBucket: import.meta.env.VITE_storageBucket,
+  //   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  //   appId: import.meta.env.VITE_appID,
+  // });
+  // const [firepadRef, setFirePadRef] = useState(null);
+  // const [firebaseApp, setFirebaseApp] = useState(initializeApp(config));
+  // const [db, setDb] = useState(getDatabase(firebaseApp));
+  // const [dbRef, setDbRef] = useState(ref(db));
 
+  const handleRefresh = () => {
+    const { dispatch, listen } = useSandpack();
+    dispatch({ type: "refresh" });
+  };
   useEffect(() => {
-    // let app = initializeApp(config);
-    // setFirePadRef(getExampleRef());
-  }, []);
-  return <div>test</div>;
+    // listens for any message dispatched between sandpack and the bundler
+    const stopListening = listen((msg) => console.log(msg));
+
+    return () => {
+      // unsubscribe
+      stopListening();
+    };
+  }, [listen]);
+  return (
+    <SandpackProvider>
+      <SandpackLayout>
+        <SandpackCodeEditor
+          showTabs={true}
+          showLineNumbers={true}
+          template="vanilla"
+          files={files}
+        />
+        <SandpackPreview showRunButton={true} />
+        <SandpackConsole />
+        {/* <SandpackCodeEditor /> */}
+        <SandpackPreview />
+      </SandpackLayout>
+      {/* <ListenerIframeMessage /> */}
+    </SandpackProvider>
+  );
 }
+
+const ListenerIframeMessage = () => {
+  const { sandpack } = useSandpack();
+
+  const sender = () => {
+    Object.values(sandpack.clients).forEach((client) => {
+      client.iframe.contentWindow.postMessage("Hello world", "*");
+    });
+  };
+
+  return <button onClick={sender}>Send message</button>;
+};
 
 // use this to validate input !
 
 // function replaceInvalidCharacters(str, replaceStr) {
 //   return str.replace(/[ ./#$[\] ]/g, replaceStr);
 // }
+
+// template for generating session keys
+// const getExampleRef = () => {
+// var ref = getDatabase().ref();
+//   var hash = window.location.hash.replace(/#/g, "");
+//   if (hash) {
+//     ref = ref.child(hash);
+//   } else {
+//     ref = ref.push(); // generate unique location.
+//     window.location = window.location + "#" + ref.key; // add it as a hash to the URL.
+//   }
+//   if (typeof console !== "undefined") {
+//     console.log("Firebase data: ", ref.toString());
+//   }
+//   return ref;
+// };
 
 export default CodeEditor;
