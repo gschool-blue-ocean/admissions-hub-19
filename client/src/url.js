8ff6991ac3ab97c3118a7cc8938173bc6300c8ca
Rane Gray <ranegray830@gmail.com>
2023-06-23 23:09:27 +0200

    troubleshooting api routing

--- client/src/url.js
+++ client/src/url.js
@@ -1,3 +1,3 @@
-const baseurl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://admissions-hub-20-production.up.railway.app'
+const baseurl = 'https://admissions-hub-20-production.up.railway.app'
 
 export default baseurl
