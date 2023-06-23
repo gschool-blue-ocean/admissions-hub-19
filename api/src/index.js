8f3e5cbf10056553a7b98841ba25107d95da77ae
Rane Gray <ranegray830@gmail.com>
2023-06-23 22:03:02 +0200

    fixed index.js file in api

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,7 @@
 import app from "./server.js";
-import { createServer } from "http";
-const server = createServer(app);
-import { Server } from "socket.io";
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Listening on port ${PORT}`);
 });
